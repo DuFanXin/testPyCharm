e6d069cc231f655fee53ef79ba183438093daf69
DuFanXin <18672969179@163.com>
2017-12-17 18:52:10 +0800

    add some code

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-  
+#====#====#====#====  
+# Project Name: 
+# Package Name:
+# File Name: 
+# Date: 
+# Copyright (c) , All Rights Reserved.
+# Author: "DuFanXin"  
+# HomePage: http://blog.csdn.net/qq_30239975  
+# Email:18672969179@163.com
+#====#====#====#==== 
---
+++ test/test.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-  
+#====#====#====#====  
+# Project Name: 
+# Package Name:
+# File Name: 
+# Date: 
+# Copyright (c) , All Rights Reserved.
+# Author: "DuFanXin"  
+# HomePage: http://blog.csdn.net/qq_30239975  
+# Email:18672969179@163.com
+#====#====#====#====
+import numpy as np
+a = np.array([[1], [2]])
+print(a)
