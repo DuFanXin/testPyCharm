2dd9001751d93a05a923ccf0a4211f290adb60fb
DuFanXin <18672969179@163.com>
2017-12-17 18:56:32 +0800

    add some code

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,7 @@
-# -*- coding:utf-8 -*-  
-#====#====#====#====  
-# Project Name: 
-# Package Name:
-# File Name: 
-# Date: 
-# Copyright (c) , All Rights Reserved.
-# Author: "DuFanXin"  
-# HomePage: http://blog.csdn.net/qq_30239975  
-# Email:18672969179@163.com
-#====#====#====#==== 
+import numpy as np
+import tensorflow as tf
+a = np.array([[1], [2]])
+print(a)
+W = tf.Variable(tf.zeros([4, 10]))
+with tf.Session() as s:
+    print(s.run(W))
