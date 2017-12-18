c6b3c8cdf4a1396a5676dfc6dc96aa6e72ffc636
yzc <18672969179@163.com>
2017-12-18 12:30:24 +0800

    delete code from Ubuntu16.04

--- test/__init__.py
+++ test/__init__.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 a = np.array([[1], [2]])
 print(a)
 W = tf.Variable(tf.zeros([4, 10]))
-with tf.Session() as s:
-    print(s.run(W))
+#with tf.Session() as s:
+#    print(s.run(W))
