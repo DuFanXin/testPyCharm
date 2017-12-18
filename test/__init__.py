8362b5668abb95380812d3330b15ea947ae6bfb0
DuFanXin <18672969179@163.com>
2017-12-18 17:32:20 +0800

    change some code

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,11 @@ import numpy as np
 import tensorflow as tf
 a = np.array([[1], [2]])
 print(a)
-W = tf.Variable(tf.zeros([4, 10]))
-#with tf.Session() as s:
-#    print(s.run(W))
+#W = tf.Variable(tf.zeros([4, 10]))
+a = tf.constant(2)
+b = tf.constant(3)
+x = tf.add(a, b)
+with tf.Session() as s:
+	writer = tf.summary.FileWriter('./graph', s.graph)
+	print(s.run(x))
+	writer.close()
