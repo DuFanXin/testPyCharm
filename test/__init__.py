7477c4efbdece0273ff28129c65c0b68cfb8847b
yzc <18672969179@163.com>
2017-12-19 14:05:19 +0800

    add u-net code

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,9 @@
 import numpy as np
+from tf_unet import unet
 import tensorflow as tf
 a = np.array([[1], [2]])
+net = unet.Unet(layers=3, features_root=64, channels=1, n_class=2)
+trainer = unet.Trainer(net)
 print(a)
 W = tf.Variable(tf.zeros([4, 10]))
 #with tf.Session() as s:
