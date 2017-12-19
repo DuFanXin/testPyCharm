d8b31b442674525d8b59c0b1dfae9f5d28b16306
DuFanXin <18672969179@163.com>
2017-12-19 14:52:13 +0800

    change some code

--- test/__init__.py
+++ test/__init__.py
@@ -1,15 +1,15 @@
 import numpy as np
-from tf_unet import unet
+#from tf_unet import unet
 import tensorflow as tf
 a = np.array([[1], [2]])
-net = unet.Unet(layers=3, features_root=64, channels=1, n_class=2)
-trainer = unet.Trainer(net)
+#net = unet.Unet(layers=3, features_root=64, channels=1, n_class=2)
+#trainer = unet.Trainer(net)
 print(a)
 #W = tf.Variable(tf.zeros([4, 10]))
 a = tf.constant(2)
 b = tf.constant(3)
 x = tf.add(a, b)
 with tf.Session() as s:
-	writer = tf.summary.FileWriter('./graph', s.graph)
+	#writer = tf.summary.FileWriter('./graph', s.graph)
 	print(s.run(x))
-	writer.close()
+	#writer.close()
